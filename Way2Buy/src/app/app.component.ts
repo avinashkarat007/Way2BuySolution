6dc0c68c0574450c2f7e10d6a9de4fdb057acc2e
Avinash K <avinashkarat007@gmail.com>
2019-03-22 10:43:24 +0530

    Angular started files added. Lets rock baby!

---
+++ Way2Buy/src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h4> Avinash's {{name}}</h4>`,
+})
+export class AppComponent  { name = 'Angular 7'; }
